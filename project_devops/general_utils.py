5926088c5a1d9446177bf2ecc4ed862bf320f95f
esharet <esharet@gmail.com>
2022-03-08 12:52:05 +0200

    add test for changing branch and listing repos in directory

--- project_devops/general_utils.py
+++ project_devops/general_utils.py
@@ -1,16 +1,20 @@
+import logging
 import argparse
 import os
+from sys import exc_info
 import git
 from prettytable import PrettyTable
 
+log = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
 
-def find_all_repos_in_directory(dir: str):
+def find_all_repos_in_directory(dir: str) -> list[git.Repo]:
     """
     find all repositories inside a directory
     """
     repos = []
-    dirs = os.listdir(args.base_dir)
-    for path in [os.path.join(args.base_dir, d) for d in dirs]:
+    dirs = os.listdir(dir)
+    for path in [os.path.join(dir, d) for d in dirs]:
         try:
             repos.append(git.Repo(path))
         except git.InvalidGitRepositoryError:
@@ -19,10 +23,36 @@ def find_all_repos_in_directory(dir: str):
     return repos
 
 
+def checkout_repos_to_new_branch(repos: list[git.Repo], branch_name: str, *, create_new_branch: bool = False) -> bool:
+    """
+    checkout all repos to a given branch name, and report the success of the operation
+    """
+    for repo in repos:
+        try:
+            if create_new_branch:
+                repo.git.checkout('-b', branch_name)
+            else:
+                repo.git.checkout(branch_name)
+        except git.GitCommandError:
+            log.error(
+                f"git checkout failed for repo {repo.working_tree_dir}, to branch '{branch_name}'", exc_info=True)
+            return False
+        return True
+
+
+def get_submodules(repo: git.Repo):
+    return(repo.submodules)
+
+
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("base_dir")
     args = p.parse_args()
 
-    print(find_all_repos_in_directory(args.base_dir))
-
+    repos = find_all_repos_in_directory(args.base_dir)
+    print(f"all repos: {repos}")
+    print("-------")
+    for repo in repos:
+        print(f"for repo {repo.working_tree_dir} : ")
+        print(repo.submodules)
+        print("-----")
---
+++ test/test_general_utils.py
@@ -0,0 +1,30 @@
+import unittest
+import git
+import os
+import project_devops.general_utils as git_utils
+
+class GeneralUtilsTest(unittest.TestCase): 
+
+    def setUp(self) -> None:
+        self.base_dir = "/home/user/projects/project_devops/test/test_repos_folder"
+        self.repos_working_dirs = [os.path.join(self.base_dir, d) for d in os.listdir(self.base_dir)]
+    
+    def test_find_all_git_repos(self,): 
+        repos_found = git_utils.find_all_repos_in_directory(self.base_dir)
+        repos_directories = [repo.working_tree_dir for repo in repos_found]
+        self.assertEqual(repos_directories, self.repos_working_dirs, "repos found isn't correct")
+    
+    def test_failure_checkout_branch(self,): 
+        test_repo = git.Repo(os.path.join(self.base_dir, "gitpython_test_repo"))
+        checkout_success = git_utils.checkout_repos_to_new_branch([test_repo], "dev", create_new_branch=True) 
+        self.assertFalse(checkout_success, "checkout to existing branch didn't failed! (although dev branch should exist) ")
+
+    def test_checkout_branch(self,): 
+        test_repo = git.Repo(os.path.join(self.base_dir, "gitpython_test_repo"))
+        checkout_success = git_utils.checkout_repos_to_new_branch([test_repo], "dev") 
+        self.assertTrue(checkout_success, "checkout to branch failed ! ")
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
